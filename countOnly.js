5d83e6446cd25d9d184c1c382692f5160bd3e7e0
Sam Yang <samyang8989@gmail.com>
2020-07-22 20:45:46 +0000

    return an object couting only the elements of input object

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const firstNames = [
+	'Karl',
+	'Salima',
+	'Agouhanna',
+	'Fang',
+	'Kavith',
+	'Jason',
+	'Salima',
+	'Fang',
+	'Joe',
+];
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+	const results = {};
+
+	for (const item of allItems) {
+		if (itemsToCount[item]) {
+			if (results[item]) {
+				results[item]++;
+			} else {
+				results[item] = 1;
+			}
+		}
+	}
+	console.log(results);
+	return results;
+};
+
+const result1 = countOnly(firstNames, {
+	Jason: true,
+	Karima: true,
+	Fang: true,
+});
+
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
+// console.log(result1);
+
+assertEqual(result1['Jason'], 1);
+assertEqual(result1['Karima'], undefined);
+assertEqual(result1['Fang'], 2);
