6b39902c265262ff9445e904fd9b1d24bc5a3c25
Sam Yang <samyang8989@gmail.com>
2020-07-22 22:01:18 +0000

    return a count of each letters of input string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const input = 'LHL';
+
+const countLetters = (string) => {
+	let result = {};
+	let joinString = string.split(' ').join('');
+
+	for (letter of joinString) {
+		if (result[letter]) {
+			result[letter]++;
+		} else {
+			result[letter] = 1;
+		}
+	}
+	console.log(result);
+	return result;
+};
+
+const resultObj = countLetters(input);
+
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
+
+assertEqual(resultObj['L'], 2);
+assertEqual(resultObj['H'], 1);
